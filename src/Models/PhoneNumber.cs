350a2d21a85a6075001227a23ccef0c51955c622
DanfordGidraph <danfordmaina@gmail.com>
2024-06-07 13:12:02 +0300

    * chore(PhoneNumber.cs): add key, required, and column annotations to PhoneNumber class properties

--- src/Models/PhoneNumber.cs
+++ src/Models/PhoneNumber.cs
@@ -1,9 +1,17 @@
-using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MPESA_V2_APIV2_MSISDN_DECRYPTER
 {
-    [Keyless]
-    public record PhoneNumber(string Hash, string Msisdn);
+    public record PhoneNumber(string Hash, string Msisdn)
+    {
+        [Key]
+        [Required]
+        [Column("hash", TypeName = "TEXT")]
+        public string Hash { get; set; } = Hash;
+
+        [Required]
+        [Column("msisdn", TypeName = "TEXT")]
+        public string Msisdn { get; set; } = Msisdn;
+    }
 }
