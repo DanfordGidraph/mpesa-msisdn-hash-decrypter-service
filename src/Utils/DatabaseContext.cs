2211b0834cc8056e44385e49246e50fb9afca695
DanfordGidraph <danfordmaina@gmail.com>
2024-06-07 13:15:00 +0300

    * fix(DatabaseContext.cs): remove the DbContextOptions<> parameter from the constructor * feat(DatabaseContext.cs): add CustomDatabaseContextFactory class for creating DbContext instances with custom database path

--- src/Utils/DatabaseContext.cs
+++ src/Utils/DatabaseContext.cs
@@ -2,15 +2,24 @@ using Microsoft.EntityFrameworkCore;
 
 namespace MPESA_V2_APIV2_MSISDN_DECRYPTER
 {
-    class DatabaseContext : DbContext // Remove the DbContextOptions<> parameter
+    public class DatabaseContext(DbContextOptions<DatabaseContext> options) : DbContext(options) // Remove the DbContextOptions<> parameter
     {
-        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
-        {
-        }
-
         public DbSet<UserRecord> Users => Set<UserRecord>();
 
         public DbSet<PhoneNumber> PhoneNumbers => Set<PhoneNumber>();
 
+    }
+
+    public class CustomDatabaseContextFactory
+    {
+        public DatabaseContext CreateDbContext(string? database_path = null)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
+            string rootDbPath = Path.Combine(Directory.GetCurrentDirectory(), "src/data/sqlite/database.sqlite");
+
+            optionsBuilder.UseSqlite(database_path ?? rootDbPath);
+
+            return new DatabaseContext(optionsBuilder.Options);
+        }
     }
 }
